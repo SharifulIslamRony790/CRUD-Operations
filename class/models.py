0c7d045d422493b708ab3a28f71f08d7627841c2
Md Shariful Islam Rony <mdsharifulislamrony790@gmail.com>
2026-02-04 14:47:28 +0600

    models update

--- class/models.py
+++ class/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    roll = models.IntegerField()
-    school = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    description = models.CharField(max_length=150)
-    addmission_date = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=False)
-    create = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now_add=True)
+    name                = models.CharField(max_length=100)
+    roll                = models.IntegerField()
+    school              = models.CharField(max_length=100)
+    address             = models.CharField(max_length=100)
+    description         = models.CharField(max_length=150)
+    addmission_date     = models.DateTimeField(auto_now_add=True)
+    is_active           = models.BooleanField(default=False)
+    create              = models.DateTimeField(auto_now_add=True)
+    update              = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.name)
